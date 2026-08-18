5498a58e2b0a1f32de335b0337a587c0c8a1f37a
PAVUKPLAY <pavuk33333@gmail.com>
2026-08-18 19:31:04 +0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from discord.ext import commands, tasks
 from config import DISCORD_TOKEN
 from arma_monitor import update_status
 from ai_chat import on_ai_message
+from event_manager import setup_event_button, reminder_task
+import asyncio
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -15,6 +17,9 @@ async def on_ready():
     print(f'🛡️ Бот {bot.user.name} успешно запущен и готов к работе!')
     if not update_status_task.is_running():
         update_status_task.start()
+    await setup_event_button(bot)
+    # Запускаем задачу напоминания в фоновом режиме
+    bot.loop.create_task(reminder_task(bot))
 
 @tasks.loop(seconds=60)
 async def update_status_task():
