10b4d175625973bb85de125823cf22823bd26c19
PAVUKPLAY <pavuk33333@gmail.com>
2026-08-20 11:32:03 +0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,6 +2,6 @@ from config import MODERATOR_ROLE_IDS
 
 def has_moderator_role(member):
     if not MODERATOR_ROLE_IDS:
-        return True  # если роли не заданы, доступ всем
+        return True
     member_role_ids = [role.id for role in member.roles]
     return any(role_id in member_role_ids for role_id in MODERATOR_ROLE_IDS)
